df20b943eff567325c894bbe95691f41956944a0
Rajveer <104855149+rajveersd13@users.noreply.github.com>
2024-01-22 18:15:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const month=document.querySelector(".month")
+const weekday=document.querySelector(".weekday")
+const day =document.querySelector(".day")
+const year =document.querySelector(".year")
+
+
+
+
+window.addEventListener("DOMContentLoaded",()=>{
+    const date = new Date()
+    console.log(date,"date")
+    console.log("month",date.getMonth()
+    console.log("weekday",date.getDay()
+    console.log("day",date.getDay()
+    )
+})
